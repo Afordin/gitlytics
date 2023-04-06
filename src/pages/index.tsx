0b7d52e597276f9fd4df51ac9fe2b6318b684f0a
JeremyDevCode <jeremydevcode@gmail.com>
2023-04-05 23:48:25 -0500

    fix: responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-screen h-full bg-[#1E1E1E]">
-      <Navbar />
-      <Header />
-      <SearchUser />
-      <Information />
-      <Footer />
-    </div>
+      <div className="w-full h-full bg-[#1E1E1E]">
+        <Navbar />
+        <Header />
+        <SearchUser />
+        <Information />
+        <Footer />
+      </div>
     </>
   );
 }
